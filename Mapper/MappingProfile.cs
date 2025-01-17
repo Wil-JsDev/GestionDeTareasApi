2d22b16a08cf6da93067d4541eba0bc0a1ada01a
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-16 20:14:58 -0400

    feat: add mapper

---
+++ Mapper/MappingProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using GestionDeTareasApi.DTos;
+using GestionDeTareasApi.Models;
+
+namespace GestionDeTareasApi.Mapper
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<CreateTaskDtos, TaskItem>()
+                    .ForMember(dest => dest.Description, src => src.MapFrom(x => x.DescriptionAboutTask))
+                    .ForMember(src => src.Status, dest => dest.MapFrom(x => x.StatusTask));
+
+            CreateMap<TaskItem, TaskDtos>();
+            CreateMap<UpdateTaskDtos, TaskItem>();
+        }
+    }
+}

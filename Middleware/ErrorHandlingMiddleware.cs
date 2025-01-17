16d6ee56ee1d29aa3bf92d47bd9e97a9a779e9c8
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-16 20:17:32 -0400

    feat: add erro handling middleware

---
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -0,0 +1,31 @@
+﻿namespace GestionDeTareasApi.Middleware
+{
+    public class ErrorHandlingMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public ErrorHandlingMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext context)
+        {
+            try
+            {
+                await _next(context);
+            }
+            catch (Exception ex)
+            {
+                context.Response.ContentType = "application/json";
+                context.Response.StatusCode = StatusCodes.Status404NotFound;
+                var response = new
+                {
+                    Error = "Id not found",
+                    stackTrace = ex.StackTrace
+                };
+                await context.Response.WriteAsJsonAsync(response);
+            } 
+        }
+    }
+}

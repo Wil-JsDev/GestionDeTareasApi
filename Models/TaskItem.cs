eac0df8fa835502a1c26495a28b2e0f6b7628ada
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-16 20:15:45 -0400

    feat: add model

---
+++ Models/TaskItem.cs
@@ -0,0 +1,15 @@
+﻿namespace GestionDeTareasApi.Models
+{
+    public sealed class TaskItem
+    {
+        public Guid Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public DateOnly DuaDate { get; set; }
+
+        public Status Status { get; set; }
+
+        public int AdditionalData { get; set; }
+    }
+}

42bf8fc39423422137dd86de64b0df9810421e22
Wilmer De La Cruz <wilmerdelacruz.dev@gmail.com>
2025-01-16 20:13:37 -0400

    feat: add db context

---
+++ Context/GestionDeTareasContext.cs
@@ -0,0 +1,31 @@
+﻿using GestionDeTareasApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace GestionDeTareasApi.Context
+{
+    public class GestionDeTareasContext : DbContext
+    { 
+        public GestionDeTareasContext(DbContextOptions<GestionDeTareasContext> contextOptions) : base(contextOptions)
+        { }
+
+        public DbSet<TaskItem> Tasks { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<TaskItem>().ToTable("Tasks");
+
+            modelBuilder.Entity<TaskItem>(task =>
+            { 
+                task.Property(x => x.Description)
+                    .HasMaxLength(maxLength: 70)
+                    .IsRequired();
+
+                task.Property(x => x.Status)
+                    .IsRequired(); 
+            });
+
+        }
+    }
+}
